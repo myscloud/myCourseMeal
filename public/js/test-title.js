262eb5dd06ec53b26aacc2c8ccd0f12ffbb7a112
Theppasith Nisitsukcharoen <tutorgaming@iTUTORs-Macbook-Air.local>
2016-04-16 17:54:35 +0700

    Add Gulp Support - run by > gulp watch

---
+++ public/js/test-title.js
@@ -0,0 +1,14 @@
+var CommentBox = React.createClass({displayName: "CommentBox",
+  render: function() {
+    return (
+      React.createElement("div", null, 
+        "This is an CommentBox KUY."
+      )
+    );
+  }
+});
+
+ReactDOM.render(
+  React.createElement(CommentBox, null),
+  document.getElementById('main')
+);
---
+++ src/test-title.js
@@ -0,0 +1,14 @@
+var CommentBox = React.createClass({
+  render: function() {
+    return (
+      <div>
+        This is an CommentBox KUY.
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(
+  <CommentBox />,
+  document.getElementById('main')
+);
