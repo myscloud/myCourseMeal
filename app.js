aff890aeeb0dd93d823d1df1a08eda819e4bb78a
Chonlathorn Kwankajornkiet <kam.chonlathorn@gmail.com>
2016-04-16 18:06:37 +0700

    for merge

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 var express = require('express');
-var bodyParser = require('body-parser');
 var app = express();
+var bodyParser = require('body-parser');
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var ObjectId = require('mongodb').ObjectID;
 
-// app.use(express.static(__dirname + '/public'));
-app.use('/', express.static(__dirname + '/public'));
+var url = 'mongodb://localhost:27017/test';
+MongoClient.connect(url, function(err, dbConnection) {
+    assert.equal(null, err);
+    db = dbConnection;
+    console.log("MongoDB: Connected correctly to server.");
+    app.listen(3000, function() {
+        console.log('Example app listening on port 3000!');
+    });
+});
 
-app.listen(3000, function() {
-    console.log('Example app listening on port 3000!');
+app.use('/static', express.static(__dirname + '/public'));
+app.use(bodyParser.json({ type: '*/*' }));
+
+app.get('/', function(req, res) {
+    res.send('Hello World!');
 });
