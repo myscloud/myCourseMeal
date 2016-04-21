6571a68681b607d28a88ff6896e710ab474b3c1a
Chonlathorn Kwankajornkiet <kam.chonlathorn@gmail.com>
2016-04-22 06:44:05 +0700

    add database/facebook api in mainApp

--- server.js
+++ server.js
@@ -16,7 +16,6 @@ var ObjectId = require('mongodb').ObjectID;
 var React = require('react')
 var Router = require('react-router')
 
-
 // Connection Setup to MongoDB
 // need to run mongod first
 var url = 'mongodb://localhost:27017/test';
@@ -43,4 +42,96 @@ app.get('*', function (req, res) {
 app.get('/', function (req, res) {
   //res.render('index');
   res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+// Database transaction
+
+app.post('/api/allParty', function(req, res) {
+  var friends = req.body.friends;
+  db.collection('party').find({owner: {$all: friends}}).toArray(function(err, doc) {
+    res.json(doc);
+  });
+});
+
+app.post('api/myParty', function(req, res) {
+  var userId = req.body.userId;
+  db.collection('party').find(
+      {$or: [{owner: userId}, {hungers: {$elemMatch: userId} }]}
+    ).toArray(function(err, doc) {
+      res.json(doc);
+    });
+});
+
+app.post('/api/viewParty', function(req, res) {
+  var partyId = req.body.partyId;
+  db.collection('party').find({_id: partyId})
+  .toArray(function(err, doc) {
+    res.json(doc);
+  });
+});
+
+app.post('/api/addParty', function(req, res) {
+  var newParty = req.body;
+  db.collection("party").insertOne(newParty, function(err, result) {
+    res.json({success: true});
+  })
+});
+
+app.post('/api/joinParty', function(req, res) {
+  var partyId = req.body.partyId;
+  var userId = req.body.userId;
+  db.collection('party').update(
+      {_id: partyId},
+      {$addToSet: {hungers: userId}},
+      function(err, result) {}
+    );
+});
+
+app.post('/api/deleteParty', function(req, res) {
+  var partyId = req.body.partyId;
+  db.collection('party').deleteOne(
+      {_id: partyId},
+      function(err, result) {}
+    );
+});
+
+app.post('/api/commentOnParty', function(req, res) {
+  var partyId = req.body.partyId;
+  var commenter = req.body.commenter;
+  var content = req.body.commentContent;
+  db.collection('party').update(
+      {_id: partyId},
+      {$addToSet: {comments: 
+        {commenter: commenter, content: content}
+      }},
+      function(err, result) {
+        var newId = result.insertedId;
+        db.collection('party').update(
+          {_id: partyId, "comments._id": newId},
+          { $currentDate: {
+            "comments.$.commentDate": true
+          }}
+          );
+        }
+    );
+});
+
+app.post('/api/kickBadGuy', function(req, res) {
+  var partyId = req.body.partyId;
+  var userId = req.body.userId;
+  db.collection('party').update(
+      {_id: partyId},
+      {$pull: {hungers: userId }},
+      function(err, result) {}
+    );
+});
+
+app.post('/api/leaveParty', function(req, res) {
+  var partyId = req.body.partyId;
+  var userId = req.body.userId;
+  db.collection('party').update(
+      {_id: partyId},
+      {$pull: {hungers: userId}},
+      function(err, result) {}
+    );
 });
--- src/mainApp.jsx
+++ src/mainApp.jsx
@@ -1,16 +1,101 @@
 import React from 'react';
+import FacebookLogin from 'react-facebook-login';
 import NavLink from './NavLink'
 class MainApp extends React.Component {
+
+    constructor() {
+      super();
+      this.initialFacebookSDK();
+      this.state = {
+        loggedIn: false,
+        userName: '',
+        userFbId: 0,
+        userImgSrc: ''
+      };
+    }
+
     render() {
         return <div>
-          <h1>myCourseMeal !! </h1>
+          <h1>myCourseMeal !hjhj! </h1>
           <ul role="nav">
           <li><NavLink to="/" onlyActiveOnIndex>Home</NavLink></li>
           <li><NavLink to="/newsfeed">newsfeed</NavLink></li>
           <li><NavLink to="/welcome">welcomepage</NavLink></li>
           </ul>
           {this.props.children}
+          
+
+          <br /><br />
+          <div id="status"></div>
+          <div>{this.state.userName}</div>
+          <img src={this.state.userImgSrc} />
+          <FacebookLogin 
+            appId="1762654480633676"
+            autoLoad={true}
+            callback={this.checkLoginState.bind(this)}
+            scope="public_profile,user_friends" /><br />
+          <button onClick={this.userLogout.bind(this)}>Log out</button>
         </div>;
+    }
+
+    handleAuthStatus(response) {
+      if(response.status == 'connected') {
+        this.setState({loggedIn: true});
+        FB.api('/me', function(response) {
+          this.setState({userName: response.name, userFbId: response.id});
+        }.bind(this));
+        FB.api('/me/picture', function(response) {
+          this.setState({userImgSrc: response.data['url']});
+        }.bind(this));
+        document.getElementById('status').innerHTML = "you are logged in!";
+      }
+      // else if(response.status == 'not_authorized')
+      else {
+        document.getElementById('status').innerHTML = "please log in";
+      }
+    }
+
+    userLogout() {
+      FB.getLoginStatus(function(response) {
+        if(response && response.status == 'connected') {
+          FB.logout(function(response) {
+            console.log("logging out...");
+          });
+        }
+      });
+    }
+
+    checkLoginState() {
+      FB.getLoginStatus(function(response) {
+        this.handleAuthStatus(response);
+      }.bind(this));
+    }
+
+    initialFacebookSDK() {
+      window.fbAsyncInit = function() {
+      FB.init({
+        appId      : 1762654480633676,
+        cookie     : true,  // enable cookies to allow the server to access 
+                            // the session
+        xfbml      : true,  // parse social plugins on this page
+        version    : 'v2.5' // use graph api version 2.5
+      });
+
+
+      FB.getLoginStatus(function(response) {
+        this.handleAuthStatus(response);
+      });
+
+      };
+
+      // Load the SDK asynchronously
+      (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));
     }
 }
 
